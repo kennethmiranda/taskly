49b71c6d906342b40898f1100b43d6e25043a71b
Kenny Miranda <kenny@lux.dev>
2024-10-09 20:38:22 -0400

    updated main page ui, added metadata

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,32 +1,52 @@
-import Logo from "@/src/app/ui/logo";
-import { lusitana } from "@/src/components/ui/fonts";
+import Logo from "@/src/components/logo";
+import { Button } from "@/src/components/ui/button";
+import { lusitana } from "@/src/components/fonts";
+import { ThemeToggle } from "@/src/components/ui/theme-toggle";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { Separator } from "@/src/components/ui/separator";
 import Link from "next/link";
+import { CardFooter } from "@/src/components/ui/card";
+
+export const metadata = {
+  title: "Task Manager and Cloud Storage System",
+  description: "Manage your tasks and store your files securely in the cloud.",
+};
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <Logo />
-      </div>
+    <main className="mt-4 gap-4 grow content-center flex min-h-screen flex-col p-6">
+      {/* Theme Toggle for testing, remove in production */}
+      <ThemeToggle />
+
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="flex flex-col justify-center gap-6 rounded-lg mx-20 px-6 py-10 md:w-2/5 md:px-20">
+          <Logo />
+          <Separator className="mt-5 mb-5" />
           <p
-            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+            className={`${lusitana.className} text-xl md:text-3xl md:leading-normal`}
           >
             <strong>
               Welcome to the Task Manager and Cloud Storage System.
             </strong>
           </p>
-          <Link
-            href="/sign-in"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Sign In</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+          <Button>
+            <Link
+              href="/sign-in"
+              className="flex items-center gap-5 rounded-lg px-6 py-3 text-sm font-medium transition-colors md:text-base"
+            >
+              Sign In
+              <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+          </Button>
+          <CardFooter className="text-muted-foreground items-center justify-center text-xs">
+            No Account?
+            <Link href="/sign-up" className="px-1.5 underline text-blue-400">
+              Sign Up
+            </Link>
+          </CardFooter>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <img src="/hero-desktop.png" alt="Task Management" />
         </div>
       </div>
     </main>

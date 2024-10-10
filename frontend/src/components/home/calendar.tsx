196216d3048b6df20a7399a512eaad70bff6de06
Kenny Miranda <kenny@lux.dev>
2024-10-10 04:47:34 -0400

    update calendar theme

--- frontend/src/components/home/calendar.tsx
+++ frontend/src/components/home/calendar.tsx
@@ -4,24 +4,39 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import { Card } from "@/src/components/ui/card";
+import { Button } from "@/src/components/ui/button";
 
 export default function Calendar() {
   return (
     <div className="calendar-container">
       <FullCalendar
+        height={"85vh"}
         plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
         headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek",
+          left: "title",
+          center: "",
+          right: "prev,next today    dayGridMonth,timeGridWeek",
+        }}
+        buttonText={{
+          today: "Current Date",
+          month: "Month View",
+          week: "Week View",
         }}
         initialView="dayGridMonth"
         nowIndicator={true}
         editable={true}
         selectable={true}
         selectMirror={true}
+        customButtons={{
+          test: {
+            text: "Test",
+            click: function () {
+              alert("test");
+            },
+          },
+        }}
         initialEvents={[
-          { title: "Test Task", start: new Date(), resourceId: "a" },
+          { title: "Test Task", start: new Date(), resourceId: "1" },
         ]}
       />
     </div>

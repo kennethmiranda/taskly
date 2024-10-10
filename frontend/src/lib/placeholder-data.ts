773c02a23e1a3d259d928e395ec86ee0ab803834
Kenny Miranda <kenny@lux.dev>
2024-10-10 00:07:58 -0400

    updated components

--- frontend/src/lib/placeholder-data.ts
+++ frontend/src/lib/placeholder-data.ts
@@ -10,7 +10,40 @@ export const users = [
   },
 ];
 
-export const tasks = [{}];
+export const tasks = [
+  {
+    id: 1,
+    title: "test1",
+    description: "test1",
+    createdAt: new Date("2024-09-25"),
+    dueDate: new Date("2024-10-05"),
+    status: "In Progress",
+  },
+  {
+    id: 2,
+    title: "Design database schema",
+    description: "Create database for the project",
+    createdAt: new Date("2024-09-26"),
+    dueDate: new Date("2024-10-03"),
+    status: "Planned",
+  },
+  {
+    id: 3,
+    title: "test3",
+    description: "test3",
+    createdAt: new Date("2024-09-27"),
+    dueDate: new Date("2024-10-10"),
+    status: "Planned",
+  },
+  {
+    id: 4,
+    title: "Write documentation",
+    description: "write docs",
+    createdAt: new Date("2024-09-24"),
+    dueDate: new Date("2024-10-08"),
+    status: "Completed",
+  },
+];
 
 export const files = [
   {

4de24b7e6390bee8e09204857dab135af42c9ef6
Kenny Miranda <kenny@lux.dev>
2024-10-11 22:02:52 -0400

    renamed file policy to privacy

--- frontend/src/app/home/privacy/page.tsx
+++ frontend/src/app/home/privacy/page.tsx
@@ -11,7 +11,7 @@ export default async function PolicyPage() {
   // await fetchTest();
 
   return (
-    <main className="flex-1 p-8 overflow-auto max-w-8xl mx-auto">
+    <main className="flex-1 p-8 max-w-8xl mx-auto">
       <h2 className="text-2xl font-bold tracking-tight">Privacy Policy</h2>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8"></div>

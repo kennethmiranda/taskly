93754ca8f347e1cf62184b9c2846c74c581a56d2
Kenny Miranda <kenny@lux.dev>
2024-10-12 00:16:15 -0400

    removed unused data

--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -1,35 +1,5 @@
 import { tasks } from "@/src/lib/placeholder-data";
 import { Task } from "@/src/lib/definitions";
-
-export async function fetchTest() {
-  console.log("Fetching test data...");
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  console.log("Data fetch completed after 3 seconds.");
-}
-
-export async function fetchTaskById(id: string): Promise<Task | undefined> {
-  const task = tasks.find((task) => task.id === id);
-  return task ? convertToTask(task) : undefined;
-}
-
-function convertToTask(task: any): Task {
-  if (!isValidStatus(task.status)) {
-    throw new Error(`Invalid status: ${task.status}`);
-  }
-  return {
-    id: task.id,
-    title: task.title,
-    createdAt: new Date(task.createdAt),
-    dueDate: new Date(task.dueDate),
-    status: task.status as Task["status"],
-    priority: task.priority,
-  };
-}
-
-function isValidStatus(status: any): status is Task["status"] {
-  return ["Planned", "In Progress", "Completed", "Cancelled"].includes(status);
-}
-
 import {
   ArrowDownIcon,
   ArrowRightIcon,
@@ -86,3 +56,9 @@ export const priorities = [
     icon: ArrowUpIcon,
   },
 ];
+
+export async function fetchTest() {
+  console.log("Fetching test data...");
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  console.log("Data fetch completed after 3 seconds.");
+}

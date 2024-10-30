b3be99266fc858007111b5af5a2bf52f22f69458
Kenny Miranda <kenny@lux.dev>
2024-10-30 19:54:59 -0400

    updated task api to function with user session

--- backend/mysql.js
+++ backend/mysql.js
@@ -1,18 +1,19 @@
-const mysql = require("mysql2");
-const dotenv = require('dotenv');
-dotenv.config({ path: 'SE2-Project/backend/.env' });
-
+const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+dotenv.config({
+  path: "/home/kennymiranda/code/SE2-Project/SE2-Project/backend/.env",
+});
+// replace with your own path to the .env.local file
 
 // Create a connection to the MySQL database
 const pool = mysql.createPool({
-  host: process.env.DBHOST,    // Use the environment variable
+  host: process.env.DBHOST, // Use the environment variable
   user: process.env.DBUSER,
   password: process.env.DBPASSWORD,
   database: process.env.DBNAME,
   waitForConnections: true, // Wait for a connection to become available
   connectionLimit: 10, // Max number of connections in pool
-  queueLimit: 0 // Max number of connection requests
+  queueLimit: 0, // Max number of connection requests
 });
-
 
 module.exports = pool;

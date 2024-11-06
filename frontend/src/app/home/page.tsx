757988d3334bad10e07b6c742f31e8a58664bcc6
avidandrade <trejoavid@gmail.com>
2024-11-05 21:41:46 -0500

    Separate logic from page.tsx for auth restrictions.

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,24 +1,12 @@
-import Calendar from "@/src/components/home/calendar";
-import { fetchTest } from "@/src/lib/data";
+// /src/app/home/page.tsx
 import { Metadata } from "next";
+import HomeContent from "@/src/app/home/homeContent";
 
 export const metadata: Metadata = {
   title: "Home | Task Manager and Cloud Storage System",
   description: "Home page",
 };
 
-export default async function HomePage() {
-  // remove in production
-  // await fetchTest();
-
-  const streak = 0;
-
-  return (
-    <main className="flex-1 p-8 max-w-8xl mx-auto">
-      <h2 className="text-2xl font-bold tracking-tight">Calendar</h2>
-      <p className="text-muted-foreground">Current Streak: {streak}</p>
-      <div className="flex items-center space-x-2"></div>
-      <Calendar />
-    </main>
-  );
+export default function HomePage() {
+  return <HomeContent />;
 }

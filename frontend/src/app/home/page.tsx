d2ad0e53c5d87d7bce0b3c6997c02550342ab570
avidandrade <trejoavid@gmail.com>
2024-11-05 21:46:01 -0500

    fixed named in import.

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,6 +1,6 @@
 // /src/app/home/page.tsx
 import { Metadata } from "next";
-import HomeContent from "@/src/app/home/homeContent";
+import HomeContent from "@/src/app/home/HomeContent";
 
 export const metadata: Metadata = {
   title: "Home | Task Manager and Cloud Storage System",

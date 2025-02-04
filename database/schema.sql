679e91caa92f13a1a11597f0f5e45b21031ec0ac
Kenny Miranda <kenny@lux.dev>
2025-02-04 01:24:49 -0500

    Fix schema.sql file

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,40 @@
+CREATE TABLE `users` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+    `name` varchar(255) DEFAULT NULL,
+    `image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+    `emailNotifications` tinyint(1) DEFAULT '0',
+    `avatar` varchar(255) DEFAULT NULL,
+    `streak` int DEFAULT '0',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+
+CREATE TABLE `user_providers` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `userId` bigint NOT NULL,
+    `provider` varchar(255) NOT NULL,
+    `providerId` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `userId` (`userId`),
+    CONSTRAINT `user_providers_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+
+CREATE TABLE `tasks` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `userId` bigint NOT NULL,
+    `title` varchar(255) DEFAULT NULL,
+    `description` text,
+    `createdAt` datetime DEFAULT NULL,
+    `dueDate` datetime DEFAULT NULL,
+    `updatedAt` datetime DEFAULT NULL,
+    `status` varchar(50) DEFAULT NULL,
+    `priority` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `userId` (`userId`),
+    CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1730237556161 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+
 CREATE TABLE `files` (
     `id` int NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
@@ -15,39 +52,4 @@ CREATE TABLE `files` (
     CONSTRAINT `files_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 306 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
 
-CREATE TABLE `tasks` (
-    `id` bigint NOT NULL AUTO_INCREMENT,
-    `userId` bigint NOT NULL,
-    `title` varchar(255) DEFAULT NULL,
-    `description` text,
-    `createdAt` datetime DEFAULT NULL,
-    `dueDate` datetime DEFAULT NULL,
-    `updatedAt` datetime DEFAULT NULL,
-    `status` varchar(50) DEFAULT NULL,
-    `priority` varchar(50) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    KEY `userId` (`userId`),
-    CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1730237556161 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
 
-CREATE TABLE `user_providers` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `userId` bigint NOT NULL,
-    `provider` varchar(255) NOT NULL,
-    `providerId` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `userId` (`userId`),
-    CONSTRAINT `user_providers_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
-
-CREATE TABLE `users` (
-    `id` bigint NOT NULL AUTO_INCREMENT,
-    `email` varchar(255) NOT NULL,
-    `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-    `name` varchar(255) DEFAULT NULL,
-    `image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-    `emailNotifications` tinyint(1) DEFAULT '0',
-    `avatar` varchar(255) DEFAULT NULL,
-    `streak` int DEFAULT '0',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
